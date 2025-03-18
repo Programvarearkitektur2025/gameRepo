83001c1401228ba044244e75567aef15d0c3620a
Mauritz Skogøy <mauritzs@stud.ntnu.no>
2025-03-18 12:25:07 +0100

    Feat: added a few root classes needed for simple logic

--- core/src/main/java/io/github/progark/Server/database/databaseManager.java
+++ core/src/main/java/io/github/progark/Server/database/databaseManager.java
@@ -4,4 +4,5 @@ package io.github.progark.Server.database;
 public interface databaseManager {
     void writeData(String key, String value);
     void readData(String key, dataCallback callback);
+    void deleteData(String key, dataCallback callback);
 }
