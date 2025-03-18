a6973cef18a6c5d56f1242ae29c2b5372deac9d6
Mauritz Skogøy <mauritzs@stud.ntnu.no>
2025-03-18 15:09:58 +0100

    Feat:UI change. First render of neww UI

--- core/src/main/java/io/github/progark/Main.java
+++ core/src/main/java/io/github/progark/Main.java
@@ -1,44 +1,28 @@
 package io.github.progark;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import io.github.progark.Client.Views.Menu.HomeView;
 import io.github.progark.Server.database.databaseManager;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class Main extends Game {
     private databaseManager dbInstance;
 
-    public Main(databaseManager dbManager){
-        if (dbManager == null){
-            return;
+    public Main(databaseManager dbManager) {
+        if (dbManager != null) {
+            dbInstance = dbManager;
         }
-        dbInstance = dbManager;
     }
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
+        this.setScreen(new HomeView(this)); // Start with the UI screen
     }
 }
