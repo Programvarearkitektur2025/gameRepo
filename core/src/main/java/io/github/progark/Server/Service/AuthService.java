f27c20ad21615b6cb3dbfd5d20978634f500395f
Stian Wilhelmsen <stianw@hotmail.no>
2025-03-18 12:25:14 +0100

    Added initial Authentication instance and Service class

---
+++ core/src/main/java/io/github/progark/Server/Service/AuthService.java
@@ -0,0 +1,9 @@
+package io.github.progark.Server.Service;
+
+public interface AuthService {
+    void signUp(String email, String password);
+    void signIn(String email, String password);
+    void signOut();
+    boolean isUserLoggedIn();
+    String getCurrentUserEmail();
+}
