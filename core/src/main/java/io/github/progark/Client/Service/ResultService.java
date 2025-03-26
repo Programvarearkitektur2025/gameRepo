acdbae5c5a899633267981a9a1f59bf8ab5b1696
Marcus Larsen-Frivoll <marcula@stud.ntnu.no>
2025-03-26 12:52:39 +0100

    Removed the fromMap and toMap from ResultService, -Controller and -Model

--- core/src/main/java/io/github/progark/Client/Service/ResultService.java
+++ core/src/main/java/io/github/progark/Client/Service/ResultService.java
@@ -1,12 +1,13 @@
 package io.github.progark.Client.Service;
 
+import io.github.progark.Client.Model.ResultModel;
+import io.github.progark.Server.database.DataCallback;
+import io.github.progark.Server.database.DatabaseManager;
+
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import io.github.progark.Server.database.DataCallback;
-import io.github.progark.Server.database.DatabaseManager;
 
 public class ResultService {
     private final DatabaseManager databaseManager;
@@ -15,48 +16,78 @@ public class ResultService {
         this.databaseManager = databaseManager;
     }
 
-    public void saveResult(String gameId,
-                           String winner,
-                           String loser,
-                           int winnerScore,
-                           int loserScore,
-                           List<String> questions,
-                           int numberOfGuessesP1,
-                           int numberOfGuessesP2,
-                           int correctGuessesP1,
-                           int correctGuessesP2,
-                           List<String> guessesP1,
-                           List<String> guessesP2,
-                           DataCallback callback) {
 
-        Map<String, Object> resultData = new HashMap<>();
-        resultData.put("winner", winner);
-        resultData.put("loser", loser);
-        resultData.put("winnerScore", winnerScore);
-        resultData.put("loserScore", loserScore);
-        resultData.put("finishedAt", new Timestamp(System.currentTimeMillis()).getTime());
+    public void saveResult(ResultModel model, DataCallback callback) {
+        String key = "results/" + model.getGameId();
 
-        resultData.put("questions", questions);
-        resultData.put("numberOfGuessesP1", numberOfGuessesP1);
-        resultData.put("numberOfGuessesP2", numberOfGuessesP2);
-        resultData.put("correctGuessesP1", correctGuessesP1);
-        resultData.put("correctGuessesP2", correctGuessesP2);
-        resultData.put("guessesP1", guessesP1);
-        resultData.put("guessesP2", guessesP2);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("gameId", model.getGameId());
+        data.put("winner", model.getWinner());
+        data.put("loser", model.getLoser());
+        data.put("winnerScore", model.getWinnerScore());
+        data.put("loserScore", model.getLoserScore());
+        data.put("finishedAt", model.getFinishedAt() != null ? model.getFinishedAt().getTime() : null);
+        data.put("questions", model.getQuestions());
+        data.put("numberOfGuessesP1", model.getNumberOfGuessesP1());
+        data.put("numberOfGuessesP2", model.getNumberOfGuessesP2());
+        data.put("correctGuessesP1", model.getCorrectGuessesP1());
+        data.put("correctGuessesP2", model.getCorrectGuessesP2());
+        data.put("guessesP1", model.getGuessesP1());
+        data.put("guessesP2", model.getGuessesP2());
 
         try {
-            databaseManager.writeData("results/" + gameId, resultData);
-            callback.onSuccess("Result saved successfully for gameId: " + gameId);
+
+            databaseManager.writeData(key, data);
+            callback.onSuccess("Result saved for " + model.getGameId());
         } catch (Exception e) {
             callback.onFailure(e);
         }
     }
 
+
     public void loadResult(String gameId, DataCallback callback) {
-        databaseManager.readData("results/" + gameId, new DataCallback() {
+        String key = "results/" + gameId;
+        databaseManager.readData(key, new DataCallback() {
             @Override
             public void onSuccess(Object data) {
-                callback.onSuccess(data);
+                if (!(data instanceof Map)) {
+
+                    callback.onFailure(new Exception("Database result is not a Map"));
+                    return;
+                }
+
+                Map<?, ?> rawMap = (Map<?, ?>) data;
+
+                ResultModel model = new ResultModel();
+
+
+                model.setGameId((String) rawMap.get("gameId"));
+                model.setWinner((String) rawMap.get("winner"));
+                model.setLoser((String) rawMap.get("loser"));
+
+
+                model.setWinnerScore(castToInt(rawMap.get("winnerScore")));
+                model.setLoserScore(castToInt(rawMap.get("loserScore")));
+
+
+                Long finishedAtLong = castToLong(rawMap.get("finishedAt"));
+                if (finishedAtLong != null) {
+                    model.setFinishedAt(new Timestamp(finishedAtLong));
+                }
+
+
+                model.setQuestions(castToStringList(rawMap.get("questions")));
+                model.setGuessesP1(castToStringList(rawMap.get("guessesP1")));
+                model.setGuessesP2(castToStringList(rawMap.get("guessesP2")));
+
+                model.setNumberOfGuessesP1(castToInt(rawMap.get("numberOfGuessesP1")));
+                model.setNumberOfGuessesP2(castToInt(rawMap.get("numberOfGuessesP2")));
+                model.setCorrectGuessesP1(castToInt(rawMap.get("correctGuessesP1")));
+                model.setCorrectGuessesP2(castToInt(rawMap.get("correctGuessesP2")));
+
+
+                callback.onSuccess(model);
             }
 
             @Override
@@ -65,5 +96,27 @@ public class ResultService {
             }
         });
     }
-}
 
+
+    private int castToInt(Object val) {
+        if (val instanceof Number) {
+            return ((Number) val).intValue();
+        }
+        return 0;
+    }
+
+    private Long castToLong(Object val) {
+        if (val instanceof Number) {
+            return ((Number) val).longValue();
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<String> castToStringList(Object val) {
+        if (val instanceof List) {
+            return (List<String>) val;
+        }
+        return null;
+    }
+}
