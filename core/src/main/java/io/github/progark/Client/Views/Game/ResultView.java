7786958732e6c221d6f77016e94a3c10573f169b
Mathilde Lykke <mathilly@stud.ntnu.no>
2025-03-28 09:41:22 +0100

    Feat: Merge corrections

--- core/src/main/java/io/github/progark/Client/Views/Game/ResultView.java
+++ core/src/main/java/io/github/progark/Client/Views/Game/ResultView.java
@@ -1,6 +1,5 @@
 package io.github.progark.Client.Views.Game;
 
-<<<<<<< HEAD
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -109,64 +108,5 @@ public class ResultView extends View {
         if (bigFont != null) {
             bigFont.dispose();
         }
-=======
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-//PLACEHOLDER RESULTVIEW, MÅ ENDRES
-public class ResultView implements Screen {
-    private Stage stage;
-    private Skin skin;
-    private Label resultLabel;
-
-    public ResultView() {
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
-
-        skin = new Skin(Gdx.files.internal("uiskin.json"));
-        resultLabel = new Label("Resultater", skin);
-
-
-        resultLabel.setPosition(100, 100);
-
-
-        stage.addActor(resultLabel);
-    }
-
-    @Override
-    public void show() {
-
-    }
-
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(delta);
-        stage.draw();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
-    }
-
-    @Override
-    public void pause() {}
-
-    @Override
-    public void resume() {}
-
-    @Override
-    public void hide() {}
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-        skin.dispose();
->>>>>>> fba5587 (Rsultmodel)
-    }
-}
+        }
+   }
