45ec14467bad652e87fe96a3863dfb050330e0c4
Mathilde Lykke <mathilly@stud.ntnu.no>
2025-03-28 11:38:19 +0100

    Restored some of the lost code in joinGameView. The code now runs

--- core/src/main/java/io/github/progark/Client/Views/Menu/JoinGameView.java
+++ core/src/main/java/io/github/progark/Client/Views/Menu/JoinGameView.java
@@ -4,15 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Scaling;
 
 import io.github.progark.Client.Views.View;
@@ -23,9 +21,9 @@ public class JoinGameView extends View {
     private final Skin skin;
     private final Texture backgroundTexture;
     private final Texture joinButtonTexture;
+    private final Texture rectangleTexture;
     private final Image background;
-    private final BitmapFont bigFont;
-    private final BitmapFont smallFont;
+    private final BitmapFont font;
     private TextField pinField;
     private Label statusLabel;
 
@@ -33,68 +31,56 @@ public class JoinGameView extends View {
         super();
         this.game = game;
         this.skin = new Skin(Gdx.files.internal("uiskin.json"));
-        
-        // Load textures
+
         this.backgroundTexture = new Texture(Gdx.files.internal("Background_1.png"));
         this.joinButtonTexture = new Texture(Gdx.files.internal("JoinButton.png"));
-        
-        // Create background
+        this.rectangleTexture = new Texture(Gdx.files.internal("Rectangle.png"));
         this.background = new Image(backgroundTexture);
-        
-        // Initialize fonts
-        this.bigFont = new BitmapFont(Gdx.files.internal("fonts/big.fnt"));
-        this.smallFont = new BitmapFont(Gdx.files.internal("fonts/small.fnt"));
+
+        // Font
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("OpenSans.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 60;
+        font = generator.generateFont(parameter);
+        generator.dispose();
     }
 
     @Override
     protected void initialize() {
-        // Set up background
         background.setFillParent(true);
         stage.addActor(background);
 
-        // Create main table
         Table table = new Table();
         table.setFillParent(true);
-        table.center().padTop(200);
+        table.top().padTop(200);
         stage.addActor(table);
 
-        // Create styles
-        Label.LabelStyle labelStyle = new Label.LabelStyle();
-        labelStyle.font = bigFont;
-        labelStyle.fontColor = Color.BLACK;
+        Label.LabelStyle labelStyle = new Label.LabelStyle(font, Color.WHITE);
+
+        // Bakgrunn for input
+        Drawable inputBackground = new TextureRegionDrawable(rectangleTexture);
 
         TextField.TextFieldStyle textFieldStyle = new TextField.TextFieldStyle();
-        textFieldStyle.font = smallFont;
-        textFieldStyle.fontColor = Color.BLACK;
-        textFieldStyle.background = skin.getDrawable("textfield");
-        textFieldStyle.cursor = skin.getDrawable("cursor");
-        textFieldStyle.selection = skin.getDrawable("selection");
+        textFieldStyle.font = font;
+        textFieldStyle.fontColor = Color.DARK_GRAY;
+        textFieldStyle.background = inputBackground;
+        textFieldStyle.cursor = skin.newDrawable("white", Color.GRAY);
 
-        // Create UI elements
         Label enterPinLabel = new Label("Enter game pin:", labelStyle);
+
         pinField = new TextField("", textFieldStyle);
-        pinField.setMessageText("Game PIN");
-        pinField.setAlignment(1); // center-aligned text
+        pinField.setMessageText("  Game PIN");
+        pinField.setAlignment(Align.center);
 
-        // Create join button
-        TextureRegionDrawable joinDrawable = new TextureRegionDrawable(joinButtonTexture);
-        ImageButton joinButton = new ImageButton(joinDrawable);
+        ImageButton joinButton = new ImageButton(new TextureRegionDrawable(joinButtonTexture));
         joinButton.getImage().setScaling(Scaling.stretch);
-        joinButton.getImage().setSize(300, 60);
-        joinButton.setSize(300, 60);
-        joinButton.invalidateHierarchy();
+        joinButton.setSize(300, 120);
 
-        // Create status label
-        statusLabel = new Label("", skin);
-        statusLabel.setFontScale(1.5f);
-
-        // Add button listener
         joinButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 String pin = pinField.getText();
                 if (!pin.isEmpty()) {
-                    // TODO: Implement game joining logic
                     System.out.println("Attempting to join game with PIN: " + pin);
                     statusLabel.setText("Joining game...");
                 } else {
@@ -103,10 +89,11 @@ public class JoinGameView extends View {
             }
         });
 
-        // Add UI elements to table
+        statusLabel = new Label("", new Label.LabelStyle(font, Color.WHITE));
+
         table.add(enterPinLabel).padBottom(60).row();
         table.add(pinField).width(550).height(100).padBottom(60).row();
-        table.add(joinButton).padBottom(20).row();
+        table.add(joinButton).padBottom(40).row();
         table.add(statusLabel);
     }
 
@@ -116,7 +103,7 @@ public class JoinGameView extends View {
         skin.dispose();
         backgroundTexture.dispose();
         joinButtonTexture.dispose();
-        bigFont.dispose();
-        smallFont.dispose();
+        rectangleTexture.dispose();
+        font.dispose();
     }
 }
