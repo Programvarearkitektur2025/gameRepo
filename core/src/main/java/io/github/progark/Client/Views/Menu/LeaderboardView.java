331a5f3c9063a7dbb5516906b9aef4cb9493ce50
Marcus Larsen-Frivoll <marcula@stud.ntnu.no>
2025-04-01 10:59:00 +0200

    Merged main into branch

---
+++ core/src/main/java/io/github/progark/Client/Views/Menu/LeaderboardView.java
@@ -0,0 +1,11 @@
+package io.github.progark.Client.Views.Menu;
+
+import io.github.progark.Client.Views.View;
+
+public class LeaderboardView extends View {
+
+    @Override
+    protected void initialize() {
+
+    }
+}
